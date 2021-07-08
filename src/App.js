bebe63cbbd3ed9ce106807398eff82178d65f501
kritika <kritikasri27@gmail.com>
2021-07-08 15:57:20 +0530

    Added edit functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ const arr = () => {
 
 function App() {
   const [item, setItem] = useState("");
+  const [edit,setEdit] =useState(false);
+  const [editId,setEditId] =useState();
   const [list, setList] = useState(arr);
+  const [error,setError] =useState("");
 
   const handleSubmit = (e) => {
     const newItem = {
@@ -23,7 +26,29 @@ function App() {
     if (item && item.length <= 25) {
       setList([...list, newItem]);
       setItem("");
+      setError("");
     }
+    else if(item && item.length <= 25 && edit)
+    {
+      setList(list.map(el=>{
+        if(el.id===editId)
+        {
+          return {...el,
+          item:item}
+        }
+        return el;
+      }
+      
+      ))
+      setItem("");
+      setEditId(null);
+      setEdit(false);
+      setError("");
+    }
+    else if(!item)
+    setError("Item cannot be blank.");
+    else if(item.length>25)
+    setError("Character limit is 25.");
   };
 
   React.useEffect(() => {
@@ -48,8 +73,7 @@ function App() {
         <button className="btn" type="submit">
           Add Items
         </button>
-        <br></br>
-        <br></br>
+       {error && <p style={{color:"red"}}>{error}</p>}
       </form>
       <div>
         {list.map((c, id) => (
@@ -61,6 +85,8 @@ function App() {
             setList={setList}
             complete={c.complete}
             setItem={setItem}
+            setEdit={setEdit}
+            setEditId={setEditId}
           />
         ))}
       </div>
